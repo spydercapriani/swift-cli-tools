232b342eacd7c5e817e0020088e90e9c4c393b08
Danny Gilbert <dgilbert@foreflight.com>
2023-06-02 10:46:43 -0500

    Initial Commit

---
+++ Sources/Demo/Demo.swift
@@ -0,0 +1,30 @@
+//
+//  Demo.swift
+//  
+//
+//  Created by Danny Gilbert on 6/1/23.
+//
+
+import ConsoleUI
+import TSCBasic
+
+@main
+struct Demo {
+  
+  static func main() throws {
+    let console = TerminalController(stream: stdoutStream)!
+    console.action("Action Title")
+    console.bot("Hello there!")
+    console.step("Step Example")
+    console.info("Information goes here")
+    console.warn("Issue warning")
+    console.error("Danger Will Robinson!")
+    console.success("Congratulations!")
+    console.wait()
+    console.confirm("Would you like to continue?") ? console.setTable() : console.flipTable()
+    let username = console.prompt("Username")
+    console.print("You entered \(username)", inColor: .yellow)
+    let password = console.prompt("Password", isSecure: true)
+    console.print("You entered \(password)", inColor: .yellow)
+  }
+}
